50b7f64abff6f72ae071b7ddb0c73cf6a85f7870
TaylorOxley <123648725+TaylorOxley@users.noreply.github.com>
2023-04-26 21:39:19 -0400

    Update men.service.ts

--- src/app/men.service.ts
+++ src/app/men.service.ts
@@ -1,4 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { men_mock_product_list } from "./women_mock_product_list"
 
-constructor(private http: HttpClient) { }
+@Injectable(
+    {provideIn: 'root'}
+)
+export class MenService {
+    private baseUrl:string = "https://hollister-app-fb5ab-default-rtdb.firebaseio.com/";
+    private productEndPoint:String = "women.json";
+
+    constructor(private http:HttpClient) {
+
+    }
+
+    getProducts() {
+        return this.http.get<men_mock_product_list []>(this.baseUrl + this.productEndPoint);  
+    }
+
+    getProduct(index:number) {
+        return this.http.get<men_mock_product_list>(this.baseUrl + 'product' + '/' + index + '.json')
+    }
+
+
+
+
+
+}
+
+ 
