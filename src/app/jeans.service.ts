ef5f0dec5ba658d703e871c8e1014405b2cffe41
TaylorOxley <123648725+TaylorOxley@users.noreply.github.com>
2023-04-26 21:37:27 -0400

    Update jeans.service.ts

--- src/app/jeans.service.ts
+++ src/app/jeans.service.ts
@@ -1,4 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { jeans_mock_product_list } from "./women_mock_product_list"
 
-constructor(private http: HttpClient) { }
+@Injectable(
+    {provideIn: 'root'}
+)
+export class JeansService {
+    private baseUrl:string = "https://hollister-app-fb5ab-default-rtdb.firebaseio.com/";
+    private productEndPoint:String = "jeans.json";
+
+    constructor(private http:HttpClient) {
+
+    }
+
+    getProducts() {
+        return this.http.get<jeans_mock_product_list []>(this.baseUrl + this.productEndPoint);  
+    }
+
+    getProduct(index:number) {
+        return this.http.get<jeans_mock_product_list>(this.baseUrl + 'product' + '/' + index + '.json')
+    }
+
+
+
+
+
+}
+
